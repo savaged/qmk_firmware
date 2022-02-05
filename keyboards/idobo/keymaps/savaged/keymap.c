9f47e636dc4b0fb7632f7a4c16ba7daba19edb5c
savaged <savagedavid@gmail.com>
2022-02-05 09:17:04 +0000

    smaller adjustment

--- keyboards/idobo/keymaps/savaged/keymap.c
+++ keyboards/idobo/keymaps/savaged/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | =      | 1      | 2      | 3      | 4      | 5      | VOL-   | MUTE   | VOL+   | 6      | 7      | 8      | 9      | 0      | -      |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | VOL-   | MUTE   | VOL+   | 6      | 7      | 8      | 9      | 0      | -      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | ESC    | Q      | W      | E      | R      | T      | INS    | DEL    | MAIL   | Y      | U      | I      | O      | P      | \      |
+ * | \      | Q      | W      | E      | R      | T      | INS    | DEL    | MAIL   | Y      | U      | I      | O      | P      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | `      | A      | S      | D      | F      | G      | LALT   | CAP LK | RALT   | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-    KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_VOLD, KC_MUTE, KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,  KC_DEL,  KC_MAIL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_VOLD, KC_MUTE, KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,  KC_DEL,  KC_MAIL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LALT, KC_CAPS, KC_RALT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_APP,  KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL, KC_PGUP, KC_PGDN, KC_BSPC, MO(_FN), KC_SPC,  KC_LGUI, KC_TAB,  KC_LGUI, KC_SPC,  MO(_FN), KC_ENT,  KC_HOME, KC_END,  KC_RCTL

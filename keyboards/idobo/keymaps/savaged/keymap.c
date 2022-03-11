2b06a4c8f8bb7d0c4cceb27d3a493dad543c01a6
savaged <savagedavid@gmail.com>
2022-03-11 20:24:25 +0000

    using mouse middle click

--- keyboards/idobo/keymaps/savaged/keymap.c
+++ keyboards/idobo/keymaps/savaged/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | VOL-   | MUTE   | VOL+   | 6      | 7      | 8      | 9      | 0      | -      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | \      | Q      | W      | E      | R      | T      | INS    | DEL    | MAIL   | Y      | U      | I      | O      | P      | =      |
+ * | \      | Q      | W      | E      | R      | T      | INS    | DEL    | Midclk | Y      | U      | I      | O      | P      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | `      | A      | S      | D      | F      | G      | LALT   | CAP LK | RALT   | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_VOLD, KC_MUTE, KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,  KC_DEL,  KC_MAIL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
+    KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,  KC_DEL,  KC_BTN3, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LALT, KC_CAPS, KC_RALT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_APP,  KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL, KC_PGUP, KC_PGDN, KC_BSPC, MO(_FN), KC_TAB,  KC_LGUI, KC_SPC,  KC_LGUI, KC_SPC,  MO(_FN), KC_ENT,  KC_END,  KC_HOME, KC_RCTL
@@ -47,23 +47,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F12    | F1     | F2     | F3     | F4     | F5     |        |        |        | F6     | F7     | F8     | F9     | F10    | F11    |
+ * | F12    | F1     | F2     | F3     | F4     | F5     | RGB-   |        | RGB+   | F6     | F7     | F8     | F9     | F10    | F11    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        | UP     |        |        |        | RGB TG | RGB HD | RGB HI |        |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        | LEFT   | DOWN   | RIGHT  |        |        |        | RGB SD | RGB SI | LEFT   | DOWN   | UP     | RIGHT  |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   |        |        |        | RGB VD | RGB VI |        |        | CALC   | MYCOMP | MAIL   |        |
+ * | PREV   | PLAY   | NEXT   | STOP   |        |        |        |        |        |        |        | CALC   | MYCOMP | MAIL   |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | VOL-   | MUTE   | VOL+   |        |        | TAB    |        | RGB RMD| RGB MD | ESC    |        |        | RALT   |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   RGB_VAD, _______, RGB_VAI, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, KC_UP,   _______, _______, _______, RGB_TOG, RGB_HUD, RGB_HUI, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_RGHT, RGB_SAD, RGB_SAI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______, KC_MSTP, RGB_VAD, RGB_VAI, _______, _______, KC_CALC, KC_MYCM, KC_MAIL, _______,
+    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______, KC_MSTP, _______, _______, _______, _______, KC_CALC, KC_MYCM, KC_MAIL, _______,
     KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_TAB,  _______, RGB_RMOD,RGB_MOD, KC_ESC,  _______, _______, KC_RALT, _______, _______
  ),
 };
